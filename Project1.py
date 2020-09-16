36d82619275cf76a5e7b7e84fcb7002cc32819e1
sleepypanda <cheeyang85@gmail.com>
2020-09-16 15:47:18 +0800

    added

--- Project1.py
+++ Project1.py
@@ -5,9 +5,12 @@ Algo lab proj 1
 
 def main():
     filename = input("Please input file location\n")
-    
+    filename = filename[1:len(filename)-2]
+    print(filename)
     genome_file = open(filename)
-    print("Make Change")
+    genome_string = genome_file.read()
+    genome_string = genome_string[genome_string.find('\n'):]
+    print(genome_string)
     
     
 if __name__ == '__main__':
