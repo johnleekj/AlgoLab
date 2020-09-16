d1380ce547652b1c26bccd924417af5ab24680ff
johnleekj <johnleekaijie@gmail.com>
2020-09-16 15:18:59 +0800

    Hello

---
+++ Project1.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Algo lab proj 1
+"""
+
+def main():
+    filename = input("Please input file location\n")
+    
+    genome_file = open(filename)
+    print("Make Change")
+    
+    
+if __name__ == '__main__':
+    main()
